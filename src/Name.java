35d538bd99025fe3b2ab9b0bcc294f415ce7f54c
Robin Luthe <robin.luthe@study-hs.duesseldorf.de>
2019-04-04 19:49:41 +0200

    generating random number

--- src/Name.java
+++ src/Name.java
@@ -1,6 +1,11 @@
 public class Name {
 
+    static double random = Math.random() * 49 + 1;
+
     public static void main(String args[]){
         System.out.println("Hi");
+
+        // new test stuff
+        System.out.println(random);
     }
 }
